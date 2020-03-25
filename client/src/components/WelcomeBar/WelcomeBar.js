8cdf1ad1c7bfbfea85d9c656a16241aaa0bc2d8e
Jack Antcio <anticoj@bc.edu>
2020-03-25 12:51:07 -0400

    Added changed to WelcomeBar component

--- client/src/components/WelcomeBar/WelcomeBar.js
+++ client/src/components/WelcomeBar/WelcomeBar.js
@@ -5,8 +5,12 @@ class WelcomeBar extends Component {
     render() {
         return (
             <div className="welcomeBar">
-              <div className="barOne"></div>
-              <div className="barTwo"></div>
+              <div className="barOne">
+                <h1 className="headerText">The Product Management Intern</h1>
+              </div>
+              <div className="barTwo">
+                <p>How to get a product management internship and what they are like</p>
+              </div>
             </div>
           );
     }
